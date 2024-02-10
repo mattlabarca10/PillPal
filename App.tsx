d1f068b37f12b3d65c27fbcd135463821bc790a0
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-02-10 13:11:15 -0500

    commented out error throwing code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput, Button, View, Image } from 'react-native';
 import { fetchData } from './api/GeminiAPI'; // Adjust the path as necessary
-import { selectImage } from './components/Image'; // Ensure this path is correct
+//import { selectImage } from './components/Image'; // Ensure this path is correct
 
-interface ImageSource {
-  uri: string;
-}
+//interface ImageSource {
+// uri: string;
+//}
 
 const App = () => {
   const [inputText, setInputText] = useState('');
@@ -18,9 +18,9 @@ const App = () => {
   };
 
   // Function to handle image selection
-  const handleSelectImage = () => {
-    selectImage((source: ImageSource) => setImageUri(source));
-  };
+  // const handleSelectImage = () => {
+  //selectImage((source: ImageSource) => setImageUri(source));
+  //};
 
   return (
     <SafeAreaView style={styles.container}>
@@ -32,9 +32,9 @@ const App = () => {
       />
       <Button title="Fetch Data" onPress={handleFetchData} />
       <Text style={styles.response}>{response}</Text>
-      <Button title="Select Image" onPress={handleSelectImage} />
+      {/* <Button title="Select Image" onPress={handleSelectImage} /> */}
       {/* Display the selected image if available */}
-      {imageUri && <Image source={imageUri} style={styles.image} />}
+      {/*imageUri && <Image source={imageUri} style={styles.image} />*/}
     </SafeAreaView>
   );
 };
@@ -60,12 +60,14 @@ const styles = StyleSheet.create({
     padding: 10,
     color: 'black',
   },
-  image: {
-    marginTop: 20,
-    width: 200, // Set the width and height as needed
-    height: 200,
-    resizeMode: 'cover', // Adjust the resize mode as needed
-  },
+  /* 
+    image: {
+      marginTop: 20,
+      width: 200, // Set the width and height as needed
+      height: 200,
+      resizeMode: 'cover', // Adjust the resize mode as needed
+    },
+     */
 });
 
 export default App;
